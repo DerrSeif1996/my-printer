7add69df91f44d3b81d0f4dbd2166a9ac3a45787
DerrSeif1996 <derrouiche.oussama31@gmail.com>
2024-08-04 18:44:35 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
      
 function printLabel() {
-      var { productName, size, Ndecarton, date } = savingvalues();
+      var { productName, size, Ndecarton, Ndepiece,date } = savingvalues();
       
       var originalContent = document.body.innerHTML;
   
@@ -14,6 +14,7 @@ function printLabel() {
       document.getElementById('product-name').value = productName;
       document.getElementById('Forma').value = size;
       document.getElementById('Ndecarton').value = Ndecarton;
+      document.getElementById('Ndepiece').value = Ndepiece;
       document.getElementById('date').value = date;
 
     attachEventListeners();
@@ -23,7 +24,7 @@ function printLabel() {
 
 
 function Alllabels(caliber){
-    var { productName, Ndecarton, date } = savingvalues();
+    var { productName, Ndecarton, Ndepiece,date } = savingvalues();
     var surface = surfacearea() + " M^2";
     var Forma = hundelforma();
     var imageURL = "images/download.png" ;
@@ -41,12 +42,16 @@ function Alllabels(caliber){
     <strong>Caliber:</strong> ${caliber} <br>
     <strong>Forma:</strong> ${Forma} <br>
     <strong>surface:</strong> ${surface} <br>
-    <strong>N de Carton:</strong> ${Ndecarton} <br>
+    <strong>N de Carton:</strong> ${Ndecarton} 
+    <strong>N de Piece:</strong> ${Ndepiece}<br>
     <strong>Date:</strong> ${date} 
 
     `;
     document.getElementById('label-content').innerHTML = labelContent;
     document.getElementById('label-preview').style.display = 'block';
+
+    
+    
 }
 
 
@@ -54,17 +59,18 @@ function savingvalues(){
     var productName = document.getElementById('product-name').value;
     var size = document.getElementById('Forma').value;
     var Ndecarton = document.getElementById('Ndecarton').value;
+    var Ndepiece = document.getElementById('Ndepiece').value;
     var date = document.getElementById('date').value;
-    return {productName,size,Ndecarton,date} ;
+    return {productName,size,Ndecarton,Ndepiece,date} ;
 }
 function surfacearea() {
-    var { size } = savingvalues();
+    var { size ,Ndecarton,Ndepiece} = savingvalues();
     var dimensions = size.split(/[^0-9]+/);
     if (dimensions.length < 2) return "Invalid size format";
     var width = parseInt(dimensions[0], 10);
     var height = parseInt(dimensions[1], 10);
     if (isNaN(width) || isNaN(height)) return "Invalid dimensions";
-    var surface = (width * height) / 10000;
+    var surface = (width/1000 * height/1000) * Ndecarton * Ndepiece;
     return surface.toFixed(2);
 }
 
