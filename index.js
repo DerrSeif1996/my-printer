b30ef3e928822ab66e53e75e5244243d96f57d0d
DerrSeif1996 <derrouiche.oussama31@gmail.com>
2024-08-04 17:30:32 +0100

    commit

--- index.js
+++ index.js
@@ -1,41 +1,84 @@
-   
-
+     
 function printLabel() {
-      // Save the values of the form fields
-      var productName = document.getElementById('product-name').value;
-      var size = document.getElementById('Forma').value;
-      var Ndecarton = document.getElementById('Ndecarton').value;
-      var date = document.getElementById('date').value;
+      var { productName, size, Ndecarton, date } = savingvalues();
       
-      // Get the original content of the page
       var originalContent = document.body.innerHTML;
   
-      // Get the label content
       var labelContent = document.getElementById('label-preview').innerHTML;
   
-      // Replace the body content with the label content
       document.body.innerHTML = labelContent;
   
-      // Print the label content
       window.print();
   
-      // Revert to the original content
       document.body.innerHTML = originalContent;
-      
-      // Restore the values of the form fields
       document.getElementById('product-name').value = productName;
       document.getElementById('Forma').value = size;
       document.getElementById('Ndecarton').value = Ndecarton;
       document.getElementById('date').value = date;
-      
 
-
-    
     attachEventListeners();
 }
 
+
+
+
+function Alllabels(caliber){
+    var { productName, Ndecarton, date } = savingvalues();
+    var surface = surfacearea() + " M^2";
+    var Forma = hundelforma();
+    var imageURL = "images/download.png" ;
+
+    var labelContent = `
+
+    <img src="${imageURL}" alt="Label Image" style="
+    display: block; 
+    width: 400px; 
+    height: auto; 
+    margin: 10px auto 0;
+    " /><br>
+
+    <strong>Nom de Model:</strong> ${productName} <br>
+    <strong>Caliber:</strong> ${caliber} <br>
+    <strong>Forma:</strong> ${Forma} <br>
+    <strong>surface:</strong> ${surface} <br>
+    <strong>N de Carton:</strong> ${Ndecarton} <br>
+    <strong>Date:</strong> ${date} 
+
+    `;
+    document.getElementById('label-content').innerHTML = labelContent;
+    document.getElementById('label-preview').style.display = 'block';
+}
+
+
+function savingvalues(){
+    var productName = document.getElementById('product-name').value;
+    var size = document.getElementById('Forma').value;
+    var Ndecarton = document.getElementById('Ndecarton').value;
+    var date = document.getElementById('date').value;
+    return {productName,size,Ndecarton,date} ;
+}
+function surfacearea() {
+    var { size } = savingvalues();
+    var dimensions = size.split(/[^0-9]+/);
+    if (dimensions.length < 2) return "Invalid size format";
+    var width = parseInt(dimensions[0], 10);
+    var height = parseInt(dimensions[1], 10);
+    if (isNaN(width) || isNaN(height)) return "Invalid dimensions";
+    var surface = (width * height) / 10000;
+    return surface.toFixed(2);
+}
+
+function hundelforma() {
+    var { size } = savingvalues();
+    var dimensions = size.split(/[^0-9]+/);
+    if (dimensions.length < 2) return "Invalid size format";
+    var width = parseInt(dimensions[0], 10);
+    var height = parseInt(dimensions[1], 10);
+    if (isNaN(width) || isNaN(height)) return "Invalid dimensions";
+    return `${width} X ${height}`;
+}
+
 function attachEventListeners() {
-    
     document.querySelectorAll('#label-form button').forEach(function(button) {
         button.addEventListener('click', function() {
             var caliber = this.innerText;
@@ -43,25 +86,3 @@ function attachEventListeners() {
         });
     });
 }
-
-
-
-function Alllabels(caliber){
-    var productName = document.getElementById('product-name').value;
-    var size = document.getElementById('Forma').value;
-    var manufacturer = document.getElementById('Ndecarton').value;
-    var date = document.getElementById('date').value;
-    
-
-    var labelContent = `
-    <strong>Nom de Model:</strong> ${productName} <br>
-    <strong>Caliber:</strong> ${caliber} <br>
-    <strong>Forma:</strong> ${size} <br>
-    <strong>N de Carton:</strong> ${manufacturer} <br>
-    <strong>Date:</strong> ${date} 
- `;
- 
- document.getElementById('label-content').innerHTML = labelContent;
- document.getElementById('label-preview').style.display = 'block';
-}
-
